ac011037259964e9494bc469cca0f883a59dcf52
goasguenl <loic.goasguen@mail-alpha.com>
2017-05-05 10:31:22 +0200

    création du dossier ressources

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/fonts'));
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/ressources'));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
