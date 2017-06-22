1c423fadc2f34de0b87145513a5835bc01ede65d
LOCHOUARN Pierre <plochoua@aslairlines.com>
2017-06-22 17:24:36 +0200

    changement cmd lancement

--- env.js
+++ env.js
@@ -1,8 +1,21 @@
-if( typeof NODE_ENV !="undefined"){
-  process.env.NODE_ENV = NODE_ENV
 
-}else{
-  process.env.NODE_ENV ="test"
 
+
+var env = process.argv[2] || 'dev';
+
+switch (env) {
+    case 'development':
+      process.env.NODE_ENV ="development"
+
+        break;
+    case 'production':
+      process.env.NODE_ENV ="production"
+        break;
+    case 'test':
+      process.env.NODE_ENV ="test"
+            break;
+    default:
+      process.env.NODE_ENV ="test"
+        break;
 }
 console.log("env : ",process.env.NODE_ENV )
