f1303654184fb374cb901fda9f4efd3c7f47f884
Pierre lochouarn <Pierre lochouarn>
2017-06-28 14:48:42 +0200

    url api

--- ressources/js/io.js
+++ ressources/js/io.js
@@ -1,11 +1,11 @@
 
 	//	connect
 
-	//var socket = io.connect('https://bman.herokuapp.com/');
-	var socket = io.connect('http://localhost:8888');
+
+	var socket = io.connect('http://bomberbot1dev.herokuapp.com');
 	console.log('check 1', socket.connected);
 	//	setup event listeners
-	
+
 
 	socket.on('welcome', function(id, playerInfo)
 	{
