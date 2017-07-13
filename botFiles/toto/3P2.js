13a1c8a639dd1e7717424705da19394fe8444c8e
FRUITET <ROBIN.FRUITET@disney.com>
2017-07-13 15:14:25 +0200

    theme on page choix

---
+++ botFiles/toto/3P2.js
@@ -0,0 +1,8 @@
+function exec(){
+    this.player2.bomb();
+    this.player2.move("right");
+    this.player2.bomb()
+    this.player2.move("left");
+    this.player2.bomb();
+    this.player2.move("down");
+}
