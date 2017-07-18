dd021c7f7ca9e900b4c1f65bfab85b5f3228ebfa
robinfruitet <rfruitet@gmail.com>
2017-07-18 11:02:41 +0200

    trophy finish

---
+++ botFiles/rfruitet/testBot.js
@@ -0,0 +1,4 @@
+function Player(t,i,n){this.context=t,this.name=i||"Whale",this.avatar=n,this.isAlive=!0,this.position={},this.maxBombs=1,this.bombs=0,this.move=function(t){},this.canGo=function(t,i){},this.clearBomb=function(){},this.plantBomb=function(){},this.render=function(t,i,n){},this.remove=function(){}};var player = new Player(null, "test", null);
+var a = 2;
+
+console.log("coucou");
