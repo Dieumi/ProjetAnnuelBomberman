e93be07da4c21c3e4cd46355a4d5892595497a60
Pierre lochouarn <Pierre lochouarn>
2017-07-19 18:57:04 +0200

    url

--- ressources/js/io.js
+++ ressources/js/io.js
@@ -3,8 +3,8 @@
 
 //var socket = io.connect('https://bman.herokuapp.com:443/');
 
-//var socket = io.connect('https://bmanserver.herokuapp.com/');
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('https://bmanserver.herokuapp.com/');
+//var socket = io.connect('http://localhost:3000/');
 console.log('check 1', socket);
 //	setup event listeners
 
@@ -206,7 +206,7 @@ socket.on('action', function () {
 
     if (gameOn != false && frozen != true) {
         socket.emit("action", player.name);
-      
+
         setTimeout(function () {
             var rand = getRandomIntInclusive(1, 5);
             if (rand > 4) {
